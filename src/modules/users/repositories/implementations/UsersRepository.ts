dd59567fdd09723e29a3512652a8572c5f15a78e
guiialmeiida <gui.almeidacrz@gmail.com>
2021-04-07 08:01:23 -0300

    Desafio ignite

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
 import { getRepository, Repository } from 'typeorm';
-
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { IFindUserByFullNameDTO, IFindUserWithGamesDTO } from '../../dtos';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../IUsersRepository';
+
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
@@ -11,20 +11,23 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  async findUserWithGamesById(data: IFindUserWithGamesDTO): Promise<User> {
+    return await this.repository.findOneOrFail(data.user_id, {
+      relations: ['games']
+    })
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM USERS ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM USERS WHERE first_name ILike $1 AND last_name ILike $2', [
+      `%${first_name}%`,
+      `%${last_name}%`,
+    ]);
   }
 }
